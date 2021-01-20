640660cab9b5d1fd4cb9b6b31e14800913683dea
abriveraaa <38618510+abriveraaa@users.noreply.github.com>
2021-01-20 22:03:23 +0800

    Update college_course.php

--- config/college_course.php
+++ config/college_course.php
@@ -2,11 +2,6 @@
 
 return [
     /**
-     * Control if the seeder should create a user per role while seeding the data.
-     */
-    'create_college' => true,
-
-    /**
      * Control if all the laratrust tables should be truncated before running the seeder.
      */
     'truncate_tables' => true,
