ba72931db83588b8abcfa6ca4a41a8c12e79f84d
abriveraaa <38618510+abriveraaa@users.noreply.github.com>
2021-01-21 16:51:05 +0800

    Update Acronym.php

--- app/Http/Traits/Acronym.php
+++ app/Http/Traits/Acronym.php
@@ -10,15 +10,16 @@ trait Acronym
      * @param string $key
      * @return string
      */
-    public function generateCollege($key, $as_space = array('_', 'of', 'and'))
+    public function generateCollege($key, $as_space = array('_', 'of', 'and', 'the', ','))
     {
 
 		$str = str_replace($as_space, '-', trim($key));
-		$str1 = str_replace('---', '-', trim($str));
+		$space = array('-----','----','---','--','-','- ', ' ');
+		$str1 = str_replace($space, '-', trim($str));
 	    $ret = '';
 	    foreach (explode('-', $str1) as $word) {
 			$ret .= strtoupper($word[0]);
-	    }
+		}
 		if($ret == "IT"){
 			$ret = "ITECH";
 		}
@@ -31,13 +32,28 @@ trait Acronym
 		$str = str_replace($as_space, '-', trim($key));
 		$space = array('--', '- ', ' ');
 		$str1 = str_replace($space, '-', trim($str));
-	    $ret = '';
+		$ret = '';
 	    foreach (explode('-', $str1) as $word) {
 			$ret .= strtoupper($word[0]);
 		}
+		
+		$result = substr($ret, 0, 2);
+		$remain = substr($ret, 2); 
 		if($ret == "BSE"){
 			$ret = "BSENTREP";
+		}elseif ($ret == "BSARCHI") {
+			$ret = "BS-ARCHI";
 		}
+		else if($ret == "BSBAMM"){
+			$ret = "BSBA-MM";
+		}elseif ($ret == "BAP") {
+			$ret = "AB-PHILO";
+		}else if($ret == "BPATA"){
+			$ret = "BPEA";
+		}else if($result == "BA"){
+			$ret = "AB".$remain;
+		}
+		// dd($ret);
 	    return $ret;
     }
 }
