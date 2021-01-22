60f317760cfa1049861017beabcea0e8b11f8ad3
abriveraaa <38618510+abriveraaa@users.noreply.github.com>
2021-01-22 09:23:46 +0800

    Create tool_seeder.php

---
+++ config/tool_seeder.php
@@ -0,0 +1,75 @@
+<?php
+
+return [
+    /**
+     * Control if all the tables should be truncated before running the seeder.
+     */
+    'truncate_tables' => true,
+    
+    'category_structure' => [
+        'Accessories' => [
+            'Alligator Chip (Black and Red)',
+            'Extension Wire',
+            'HDMI',
+            'Power Line Cord',
+            'Test Probe',
+            'VGA',
+        ],
+        'Bench_Power_Supply' => [
+            'DC Power Supply',
+        ],
+        'Carpentry_Tools' => [
+            'Hammer',
+            'Metal Scissor',
+            'Metal Saw (Stanley)',
+            'Metal Saw (Unknown)',
+            'Tape Measure',
+            'Wood Saw',
+        ],
+        'Driving_Tools' => [
+            'Adjustable Wrench',
+            'Diagonal Cutter Plier (N)',
+            'Diagonal Cutter Plier (O)',
+            'Diagonal Plier (N)',
+            'Flaring Tools (Copper Tube)',
+            'Long Nose Plier (O)',
+            'Screw Driver (Phillips)',
+            'Screw Driver (Slotted)',
+            'Socket Wrench Set (16 pcs - Hans)',
+            'Socket Wrench Set (18 pcs - Hans)',
+            'Vice Grip',
+        ],
+        'IT_Equipment' => [
+            'Creative Pen & Touch Tablet (Small)',
+            'Creative Pen & Touch Tablet (Medium)',
+            'Internet Modem',
+            'Keyboard',
+            'Mouse',
+        ],
+        'Projector' => [
+            'Projector',
+        ],
+        'Reworks_Station' => [
+            'SMD Reworks Station',
+        ],
+        'Soldering_Iron' => [
+            'Soldering Gun (30 Watts)',
+            'Soldering Iron (60 Watts)',
+        ],
+        'Test_Equipment' => [
+            'Analogue Multi Tester (Sanwa)',
+            'Analogue Multi Tester (SEW)',
+            'Analogue Multi Tester (Standard)',
+            'Cable Tester',
+            'Digital Multi Tester (Sanwa)',
+            'Digital Multi Tester (UNI-T)',
+        ],
+        'Test_Instruments' => [
+            'Audio Generator/ Counter',
+            'AC Milli-Ammeter',
+            'Digital Storage Oscilloscope',
+            'Function Generator',
+            'Signal Generator/ Counter',
+        ],
+    ],
+];
