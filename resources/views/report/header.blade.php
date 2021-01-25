bec6b5746683eb7361360ce73610d5740cd74e35
abriveraaa <38618510+abriveraaa@users.noreply.github.com>
2021-01-25 08:38:20 +0800

    Create header.blade.php

---
+++ resources/views/report/header.blade.php
@@ -0,0 +1,20 @@
+<table width="100%"  cellspacing="1" cellpadding="0"; style="border-collapse: collapse; line-height:1;">
+    <tr>
+        <td rowspan="5" style="width: 10%;"><img src="http://localhost/tems/public/img/pup_logo.png"/></td>
+        <td rowspan="5" style="width: 1%;"></td>
+        <td style="font-family: Calibri; font-size: 11pt">Republic of the Philippines</td>
+    </tr>
+    <tr>
+        <td style="font-size: 11pt;font-family: Californian FB;"><span style="font-size: 14pt;">P</span>OLYTECHNIC <span style="font-size: 14pt;">U</span>NIVERSITY OF THE <span style="font-size: 14pt;">P</span>HILIPPINES</td>
+    </tr>
+    <tr>
+        <td style="font-size: 11pt;font-family: Californian FB;"><span style="font-size: 14pt;">O</span>FFICE OF THE <span style="font-size: 14pt;">V</span>ICE <span style="font-size: 14pt;">P</span>RESIDENT FOR <span style="font-size: 14pt;">A</span>CADEMIC <span style="font-size: 14pt;">A</span>FFAIRS</td>
+    </tr>
+    <tr>
+        <td style="font-size: 15pt;font-family: Californian FB;font-weight:400">INSTITUTE OF TECHNOLOGY</td>
+    </tr>
+    <tr>
+        <td style="font-size: 11pt;font-family: Californian FB;">LABORATORY OFFICE</td>
+    </tr>
+</table>
+<hr style="border: 1pt solid black;">
