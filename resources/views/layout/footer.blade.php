78c2bb213a208a2f90bd3e5d2c283f280117bc27
abriveraaa <38618510+abriveraaa@users.noreply.github.com>
2021-01-25 10:33:39 +0800

    Create footer.blade.php

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,7 @@
+<div class="float-left d-none d-sm-block">
+    <strong>Copyright © {{ now()->year }}.</strong> {{ config('adminlte.name') }}.
+</div>
+<div class="float-right d-none d-sm-block">
+    {{-- <strong><a href="#">{{ config('adminlte.name') }}</a></strong> --}}
+    <strong>Build v{{ Illuminate\Foundation\Application::VERSION }}</strong>
+</div>
