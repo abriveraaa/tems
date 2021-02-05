2999bd74ffe067a2e49853ec1e72966f227e7246
abriveraaa <38618510+abriveraaa@users.noreply.github.com>
2021-02-05 09:09:13 +0800

    Update course-index.blade.php

--- resources/views/borrower/course-index.blade.php
+++ resources/views/borrower/course-index.blade.php
@@ -81,7 +81,7 @@
     @endpermission
 @stop
 @section('footer')
-   @include('layout.footer)
+   @include('layout.footer')
 @stop
 @section('css')
     <link rel="stylesheet" href="{{ asset('client/css/custom.css') }}">
