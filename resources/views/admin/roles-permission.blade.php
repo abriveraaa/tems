de709bf878687bc0983f25372d9a99c3dfc30d07
abriveraaa <arielbunagrivera@gmail.com>
2021-02-11 15:25:14 +0800

    changed the spelling of line 44

--- resources/views/admin/roles-permission.blade.php
+++ resources/views/admin/roles-permission.blade.php
@@ -41,7 +41,7 @@
                                             <tr>
                                                 <th>Name</th>
                                                 <th># Roles</th>
-                                                <th># Permission</th>
+                                                <th># Permissions</th>
                                                 <th></th>
                                             </tr>
                                         </thead>
